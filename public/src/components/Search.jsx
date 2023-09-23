eff13c0ae7a12af6e59654e7283b1149c735370e
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-09-23 14:15:49 +0200

    Search completed

--- public/src/components/Search.jsx
+++ public/src/components/Search.jsx
@@ -1,27 +1,48 @@
 import { useState } from 'react'
 import Words from './Words.jsx'
 import Word from './Word.jsx'
+import { words } from '../constants/words.js'
+
+// normalize text 
+const normalizeText = (text) => {
+    return text
+        .toLowerCase()
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+}
 
 const Search = () => {
     const [search, setSearch] = useState('')
+    const [wordsSearched, setWordsSearched] = useState([])
 
     const handleChange = (event) => {
-        setSearch(event.target.value)
+        const searchTerm = normalizeText(event.target.value)
+        setSearch(searchTerm)
+
+        // filter key word to search them
+        const filteredWords = words.filter(word => normalizeText(word.word).includes(searchTerm));
+        setWordsSearched(filteredWords);
     }
 
     return (
         <>
             <main>
-                <input 
-                    type="text" 
-                    placeholder="Buscar la palabra" 
-                    className="search" 
+                <input
+                    type="text"
+                    placeholder="Buscar la palabra"
+                    className="search"
                     value={search}
                     onChange={handleChange}
                 />
             </main>
             {!search && <Words />}
-            
+            {search && (
+                <section>
+                    {wordsSearched.map((ws, index) => (
+                        <Word key={index} title={ws.word} description={ws.description} />
+                    ))}
+                </section>
+            )}
         </>
     )
 }
